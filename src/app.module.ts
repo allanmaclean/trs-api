96b977340bcfeee11f3922a67c52def303d94065
Allan MacLean <allanmaclean@Allans-MacBook-Pro-2.local>
2025-09-17 21:41:58 -0400

    add db sqlite and tweak typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Moves } from './moves.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([Moves]),
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'database.sqlite',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      entities: [Moves],
       synchronize: true, // set to false in production!
     }),
     // ...other modules
