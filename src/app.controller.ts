c5f4eaaf698fffe02716c39cc32ff95d69e2812e
Allan MacLean <allanmaclean@Allans-MacBook-Pro-2.local>
2025-09-18 12:33:57 -0400

    tweak position logic to include direction

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,10 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// In an instance where types are shared between frontend and backend, there's various solutions such as shared npm types packages
+const DIRECTIONS = ['NORTH', 'EAST', 'SOUTH', 'WEST'];
+export type Direction = typeof DIRECTIONS[number];
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -11,8 +15,13 @@ export class AppController {
   }
 
   @Post('position')
-  updatePosition(@Body() position: { x: number; y: number }): string {
-    console.log('Position updated:', position);
-    return 'Position updated';
+  updatePosition(@Body() position: { x: number; y: number, direction: Direction }): Promise<string> {
+
+    return this.appService.updatePosition(position.x, position.y, position.direction).then(() => {
+      return 'Position updated';
+    }).catch((error) => {
+      console.error('Error updating position:', error);
+      throw error;
+    });
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Moves } from './moves.entity';
 import { Repository } from 'typeorm';
+import { Direction } from './app.controller';
 
 @Injectable()
 export class AppService {
@@ -12,10 +13,18 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
-  updatePosition(x: number, y: number): Promise<Moves> {
-    console.log('Position updated:', { x, y });
-    const move = this.movesRepository.create({ move: `PLACE ${x},${y}` });
-    return this.movesRepository.save(move);
-    // return 'Position updated';
+  async updatePosition(x: number, y: number, direction: Direction): Promise<Moves> {
+    // console.log('Position updated:', { x, y });
+    // const move = this.movesRepository.create({ move: `PLACE ${x},${y}` });
+    // return await this.movesRepository.save(move);
+    // // return 'Position updated';
+    try {
+      console.log('Position updated:', { x, y, direction });
+      const move = this.movesRepository.create({ move: `PLACE ${x},${y},${direction}` });
+      return await this.movesRepository.save(move);
+    } catch (error) {
+      console.error('Error saving move:', error);
+      throw error;
+    }
   }
 }
